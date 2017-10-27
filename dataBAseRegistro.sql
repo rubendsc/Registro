b2fbc56d63a82173b0cda9f02eb2a8d5fd253b6d
rubendsc <30706327+rubendsc@users.noreply.github.com>
2017-10-26 19:40:21 -0500

    Update dataBAseRegistro.sql

--- dataBAseRegistro.sql
+++ dataBAseRegistro.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS RegistroDB;
 
+USE RegistroDB;
+
 CREATE TABLE IF NOT EXISTS RegistroDB.usuarioRegistro (
     nombre VARCHAR(16) NOT NULL,
     apellido VARCHAR(16) NOT NULL,
